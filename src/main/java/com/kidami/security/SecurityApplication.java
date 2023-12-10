ce0f0892e456a57beb923f3d58970859e931426f
Allatchimi <kellanamine@gmail.com>
2023-12-10 02:18:07 +0100

    deployement du projet

---
+++ src/main/java/com/kidami/security/SecurityApplication.java
@@ -0,0 +1,13 @@
+package com.kidami.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityApplication.class, args);
+	}
+
+}
