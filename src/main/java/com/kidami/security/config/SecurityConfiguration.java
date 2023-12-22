6bae2ebda4aa5f3f8a8dc2b056db082ff5c63af6
Allatchimi <kellanamine@gmail.com>
2023-12-22 13:07:39 +0100

    mise a jour des package depreciad

--- src/main/java/com/kidami/security/config/SecurityConfiguration.java
+++ src/main/java/com/kidami/security/config/SecurityConfiguration.java
@@ -1,36 +1,40 @@
 package com.kidami.security.config;
 
+import com.kidami.security.filters.JwtRequestFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfiguration {
-    //private final JwtAuthenticationFilter jwtAuthFilter;
-    //
-    //
-    // private final AuthenticationProvider authenticationProvider;
+    @Autowired
+    private JwtRequestFilter requestFilter;
 
-//    private static final String[] AUTH_WHITELIST = {
-//            // General
-//            "/",
-//            // Auth
-//            "/api/v1/auth/**",
-//            // Swagger UI
-//            "/api-docs",
-//            "/api-docs/**",
-//            "/swagger-ui.html",
-//            "/swagger-ui/**",
-//            "/register"
-//    };
+    private static final String[] AUTH_WHITELIST = {
+            // General
+            "/",
+            // Auth
+            "/api/v1/auth/**",
+            // Swagger UI
+            "/api-docs",
+            "/api-docs/**",
+            "/swagger-ui.html",
+            "/swagger-ui/**",
+            "/register"
+    };
 
     @Bean
     public PasswordEncoder PasswordEncoder() {
@@ -40,23 +44,25 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-//        http.cors(AbstractHttpConfigurer::disable);
-//        http.csrf(AbstractHttpConfigurer::disable);
-//        http.authorizeHttpRequests(request -> {
-//            request.requestMatchers("/register").permitAll();
-//            request.anyRequest().authenticated();
-//        });
-//        return http.build();
+       return http
+               .csrf(AbstractHttpConfigurer::disable)
+               .authorizeHttpRequests((authorize) -> authorize
+                       .requestMatchers("/register","/v3/api-docs/**","/authentication","/swagger-ui/**").permitAll()
+                       .requestMatchers("/api/**")
+                       .authenticated()
+                       .anyRequest().authenticated()
+               )
+               .sessionManagement((sessionManagement)->
+                       sessionManagement
+                               .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 
-       return http.csrf().disable()
-               .authorizeHttpRequests()
-               .requestMatchers("/register","/v3/api-docs/**","/swagger-ui/**").permitAll()
-               .and()
-               .authorizeHttpRequests().requestMatchers("/api/**")
-               .authenticated().and()
-               .sessionManagement()
-               .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-               .and().build();
+               )
+               .addFilterBefore(requestFilter, UsernamePasswordAuthenticationFilter.class)
+               .build();
 
+    }
+    @Bean
+    public AuthenticationManager authenticationManager (AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
     }
 }
