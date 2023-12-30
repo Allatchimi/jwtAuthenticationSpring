b2433134abd285209c8f60951803afe13452d2f6
Allatchimi <kellanamine@gmail.com>
2023-12-30 23:45:26 +0100

    register et login stable sans spring security

---
+++ src/main/java/com/kidami/security/services/impl/AuthServiceImpl.java
@@ -0,0 +1,52 @@
+package com.kidami.security.services.impl;
+
+import com.kidami.security.dto.LoginDTO;
+import com.kidami.security.dto.RegisterDTO;
+import com.kidami.security.exception.BlogAPIException;
+import com.kidami.security.models.User;
+import com.kidami.security.repository.UserRepository;
+import com.kidami.security.services.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class AuthServiceImpl implements AuthService {
+
+    @Autowired
+    private UserRepository userRepository;
+    @Override
+    public String login(LoginDTO loginDTO) {
+
+        LoginDTO login = new LoginDTO(loginDTO.getUsernameOrEmail(),loginDTO.getPassword());
+
+        return "connected";
+    }
+
+    @Override
+    public String register(RegisterDTO registerDTO) {
+        // add check for username exists in database
+        if(userRepository.existsByFirstname(registerDTO.getFirstname())){
+            throw new BlogAPIException(HttpStatus.BAD_REQUEST, "Username is already exists!.");
+        }
+
+        // add check for email exists in database
+        if(userRepository.existsByEmail(registerDTO.getEmail())){
+            throw new BlogAPIException(HttpStatus.BAD_REQUEST, "Email is already exists!.");
+        }
+
+
+        User user = new User();
+        user.setFirstname(registerDTO.getFirstname());
+        user.setLastname(registerDTO.getLastname());
+        user.setEmail(registerDTO.getEmail());
+        user.setRole(registerDTO.getRole());
+        user.setPassword(registerDTO.getPassword());
+        //user.setPassword(passwordEncoder.encode(registerDTO.getPassword()));
+
+        userRepository.save(user);
+
+        return "User registered successfully!.";
+    }
+}
